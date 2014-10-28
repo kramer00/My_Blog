426ea59f775c84ac19b1f1efb4bbc1a3b5a3eb5f
kramer00 <jeffkramer00@gmail.com>
2014-10-28 10:21:23 -0700

    update_header

--- admin/list_users.php
+++ admin/list_users.php
@@ -1,17 +1,18 @@
-<?php include_once 'admin_header.php'; ?>
+<?php
+include_once 'admin_header.php';
+ ?>
 <?php
 include_once '../backend/user_functions.php';
 
 $users = get_user();
-
 ?>
 <style>
-	.glyphicon-remove {
-		color: red;
-		font-size: 18px;
-		font-weight: bold;
-		cursor: pointer;
-	}
+    .glyphicon-remove {
+        color: red;
+        font-size: 18px;
+        font-weight: bold;
+        cursor: pointer;
+    }
 </style>
     <!-- Page Heading -->
     <div class="row">
@@ -40,32 +41,34 @@ $users = get_user();
                 foreach($users as $user) {
                     ?>
                     <tr>
-                        <td><?php echo $user['user_id'];?></td>
-                        <td><?php echo $user['username'];?></td>
-                        <td><?php echo $user['email'];?></td>
+                        <td><?php echo $user['user_id']; ?></td>
+                        <td><?php echo $user['username']; ?></td>
+                        <td><?php echo $user['email']; ?></td>
                         <td><span user-id="<?php echo $user['user_id']; ?>" class="glyphicon glyphicon-remove remove-user"></span> | Edit</td>
                     </tr>
                 <?php
-                }
+				}
                 ?>
             </table>
         </div>
     </div>
     <!-- /.row -->
 <script>
-	$('.remove-user').click(function() {
-			var confirm_result = confirm('Are you sure you want to delete this clown?');
-			if (confirm_result) {
-				var blah = $(this);
-				var data = {
-					id : blah.attr('user-id')
-				}
-				$.post('/ajax/delete_user.php', data, function(response) {
-					if (response == 1) {
-						blah.parent().parent().remove();
-					}
-				});
-			}
-		});
+    $('.remove-user').click(function() {
+        var confirm_result = confirm('Are you sure you want to delete this clown?');
+        if (confirm_result) {
+            var blah = $(this);
+            var data = {
+                id : blah.attr('user-id')
+            }
+            $.post('/ajax/delete_user.php', data, function(response) {
+                if (response == 1) {
+                    blah.parent().parent().remove();
+                }
+            });
+        }
+    });
 </script>
-<?php include_once 'admin_footer.php'; ?>
+<?php
+include_once 'admin_footer.php';
+ ?>
