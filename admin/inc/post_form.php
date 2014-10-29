fde9e9451bd1a11ca467d097caa5ffbf8ef0cf60
kramer00 <jeffkramer00@gmail.com>
2014-10-29 14:46:45 -0700

    postFormUpdate

--- admin/inc/post_form.php
+++ admin/inc/post_form.php
@@ -9,6 +9,14 @@
                 <textarea name="body" class="form-control" id="body"></textarea>
             </div>
             <div class="form-group">
+            	<input type="hidden" name="original_picture" value="<?php echo $post['picture']; ?>" />
+            	<?php
+            	
+            	if(!empty($post[picture])) { ?>
+            		<a href="/img/<?php echo $post['picture']; ?>" target="_new">Picture Goes Here</a>
+            		<?php
+            	}
+            	?>
             	<label for="photo">add file</label>
             	<input type="file" name="photo" id="photo">
             </div>
@@ -16,3 +24,21 @@
                 Post
             </button>
         </form>
+        
+<script>
+	var post id =<?php echo $_GET['id']; ?>;
+	var user id =<?php echo $_SESSION['user']['user_id']; ?>;
+	
+	var comment;
+	$(function() {
+		$('#comment-form').submit(function(){
+			comment = $('#comment').val();
+			
+			var data = {
+				'post_id': post_id,
+				'user_id': user_id,
+				'comment': comment
+			}
+		});
+	});
+</script>
